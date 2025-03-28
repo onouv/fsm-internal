b8f8547932d9287e80bdabf7995a72af1199d4a1
olaf <onouv@t-online.de>
2025-03-28 14:51:13 +0100

    adding explanations as comments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+
+/// Business operation results, here modeled as empty structs
 #[derive(Debug)]
 struct A;
 #[derive(Debug)]
@@ -5,12 +7,9 @@ struct B;
 #[derive(Debug)]
 struct C;
 
-
-enum Event {
-    E1,
-    E2
-}
-
+/// Return type for the FSM transition function,
+/// cavering all possible different results from the 
+/// business operations  
 #[derive(Debug)]
 enum Result {
     A(A),
@@ -26,11 +25,19 @@ enum State {
     S3
 }
 
+enum Event {
+    E1,
+    E2
+}
+
+/// The finite state machine
 #[derive(Debug)]
 struct Stateful {
     state: State
 }
 impl Stateful {
+
+    /// initial default state 
     pub fn new() -> Self {
         Self {
             state: State::S0
@@ -41,14 +48,20 @@ impl Stateful {
         self.state
     }
 
-    // S0 -E1-> S1(A) -E2-> S2(B) -E1-> S3(C)
-    //          S1    -E1->             S3(A)
+    /// The fsm transition function. All possible transitions are 
+    /// modeled here. Also, all guard logic must be modelled here. 
+    /// Business methods must be called from here and the results must be 
+    /// wrapped in the common return type.
+    /// Implements the following transitions: 
+    /// S0 -E1-> S1(A) -E2-> S2(B) -E1-> S3(C)
+    ///          S1    -E1->             S3(A)
+    /// all other transitions are suppressed and deliver 'None'.
+
     pub fn next_state(&mut self, event: Event) -> Option<Result> {
         match (self.state, event) {
             (State::S0, Event::E1) => {
-                
                 self.state = State::S1;
-                return Some(Result::A(A{}));
+                return Some(Result::A(self.do_business()));
             }
             (State::S1, Event::E1) => {
                 self.state = State::S3;
@@ -63,7 +76,12 @@ impl Stateful {
                 return Some(Result::A(A{}));
             },
             _ => None
-        }        
+        }        type 
+    }
+
+    /// Business methods ...
+    fn do_business(&self) -> A {
+        return A {};
     }
 }
 
